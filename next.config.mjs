b0cf83ed5c354e7e8105ed005fdfd06261f0c670
Muskan Fatima <160476952+muskan-fatim@users.noreply.github.com>
2025-02-13 19:31:58 +0500

    Update
    add domain name of image

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-  domains:["cdn.sanity.io"]
+  domains:["cdn.sanity.io","media-hosting.imagekit"]
     },
     eslint:{
         ignoreDuringBuilds:true
