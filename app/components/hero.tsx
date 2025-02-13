3b512d060f091f6101907d6a54e0c9ef74f3321b
muskan fatim <abdulrehmanali2626@gmail.com>
2025-02-13 16:59:41 +0500

    update

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -5,26 +5,23 @@ export default function Hero() {
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
-    // Trigger the animation after component mounts
     setIsVisible(true);
   }, []);
 
   return (
     <section className="h-screen bg-gradient-to-tr from-[#1565C0] to-[#E3F2FD] text-white flex items-center justify-center">
       <div className="text-center px-4 sm:px-6 md:px-12 lg:px-24 flex flex-col sm:flex-row items-center">
-        {/* Image Section (Mobile View) */}
         <div
           className={`sm:w-1/2 mt-6 sm:mt-0 sm:pl-6 md:hidden transition-opacity duration-1000 ease-out ${
             isVisible ? "opacity-100" : "opacity-0"
           }`}
         >
           <img
-            src="main_picture.jpeg" // Replace with your image URL
+            src="https://media.licdn.com/dms/image/v2/D4E35AQGuYCbJWYSQFA/profile-framedphoto-shrink_800_800/profile-framedphoto-shrink_800_800/0/1732752815457?e=1740052800&v=beta&t=wH-mtK36dqVUtmCpQRzms7TV5wN5NmaBWqxQjwtbCVY" 
             alt="Abdul Munim Arain"
             className="w-3/4  max-w-xs mx-auto rounded-full mt-7 shadow-xl shadow-black border-2 border-white"
           />
         </div>
-        {/* Text Section */}
         <div
           className={`sm:w-1/2 transform transition duration-1000 ease-out ${
             isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-6"
@@ -52,7 +49,7 @@ export default function Hero() {
           }`}
         >
           <img
-            src="main_picture.jpeg" // Replace with your image URL
+            src="https://media.licdn.com/dms/image/v2/D4E35AQGuYCbJWYSQFA/profile-framedphoto-shrink_800_800/profile-framedphoto-shrink_800_800/0/1732752815457?e=1740052800&v=beta&t=wH-mtK36dqVUtmCpQRzms7TV5wN5NmaBWqxQjwtbCVY" 
             alt="Abdul Munim Arain"
             className="w-full max-w-xs mx-auto rounded-full shadow-xl shadow-black border-2 border-white bg-black"
           />
